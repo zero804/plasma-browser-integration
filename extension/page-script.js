ecf553f3f0a5164b142839d90856c66df10ba55b
Fabian Vogt <fabian@ritter-vogt.de>
2020-08-23 10:43:50 +0200

    Add back lost comments

--- extension/page-script.js
+++ extension/page-script.js
@@ -212,6 +212,18 @@
                 };
             }
 
+            // here we replace the document.createElement function with our own so we can detect
+            // when an <audio> tag is created that is not added to the DOM which most pages do
+            // while a <video> tag typically ends up being displayed to the user, audio is not.
+            // HACK We cannot really pass variables from the page's scope to our content-script's scope
+            // so we just blatantly insert the <audio> tag in the DOM and pick it up through our regular
+            // mechanism. Let's see how this goes :D
+
+            // HACK When removing a media object from DOM it is paused, so what we do here is once the
+            // player loaded some data we add it (doesn't work earlier since it cannot pause when
+            // there's nothing loaded to pause) to the DOM and before we remove it, we note down that
+            // we will now get a paused event because of that. When we get it, we just play() the player
+            // so it continues playing :-)
             let addPlayerToDomEvadingAutoPlayBlocking = function(player) {
                 player.registerInDom = () => {
                     // Needs to be dataset so it's accessible from mutation observer on webpage
@@ -258,6 +270,9 @@
                 return createdTag;
             };
 
+            // We also briefly add items created as new Audio() to the DOM so we can control it
+            // similar to the document.createElement hack above since we cannot share variables
+            // between the actual website and the background script despite them sharing the same DOM
             var oldAudio = window.Audio;
             window.Audio = function(...args) {
                 const player = new oldAudio(...args);
